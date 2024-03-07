d249de722fbf13bfed1421a9d2e2d2d2d6aee841
ana <156020885+makia014@users.noreply.github.com>
2024-03-07 14:42:47 +0200

    Create a 16x16 grid of squares with js and css

--- script.js
+++ script.js
@@ -1,1 +1,15 @@
-console.log("test");
+const grid = document.querySelector(".squares");
+const n = 16;
+i = 0;
+numberOfSquares = n * n;
+while (i <= numberOfSquares) {
+  const row = document.createElement("div");
+  row.classList.add("row", "flex");
+  for (let j = 0; j < n; j++) {
+    let div = document.createElement("div");
+    div.classList.add("square");
+    row.appendChild(div);
+  }
+  grid.appendChild(row);
+  i += n;
+}
