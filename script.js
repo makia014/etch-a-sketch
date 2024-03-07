f3e74f9a7e93dd1776fea2879f0004419bea7577
ana <156020885+makia014@users.noreply.github.com>
2024-03-08 00:25:46 +0200

    Add button to set grid size

--- script.js
+++ script.js
@@ -1,19 +1,47 @@
 // create a grid of squares
-const grid = document.querySelector(".squares");
-const n = 16;
-i = 0;
-const numberOfSquares = n * n;
-while (i <= numberOfSquares) {
-  const row = document.createElement("div");
-  row.classList.add("row", "flex");
-  for (let j = 0; j < n; j++) {
-    let div = document.createElement("div");
-    div.classList.add("square");
-    row.appendChild(div);
+const createGrid = (numberOfSquares) => {
+  const grid = document.querySelector(".squares");
+
+  // empty grid before creating new one
+  while (grid.firstChild) {
+    grid.removeChild(grid.lastChild);
   }
-  grid.appendChild(row);
-  i += n;
-}
+
+  i = 0;
+
+  totalSquares = numberOfSquares * numberOfSquares;
+  while (i <= totalSquares) {
+    const row = document.createElement("div");
+    row.classList.add("row", "flex");
+    for (let j = 0; j < numberOfSquares; j++) {
+      let div = document.createElement("div");
+      div.classList.add("square");
+      row.appendChild(div);
+    }
+    grid.appendChild(row);
+    i += numberOfSquares;
+  }
+};
+
+// get number of squares per side for the grid;
+const setGrid = () => {
+  const setNumber = document.querySelector(".set");
+  setNumber.addEventListener("click", () => {
+    const n = parseInt(
+      prompt("How many squares per side? (no more than 100 please)", "")
+    );
+
+    if (!n || n > 100 || n < 4) {
+      createGrid(16);
+      setHoverEffect();
+    } else {
+      createGrid(n);
+      setHoverEffect();
+    }
+  });
+};
+createGrid(16);
+setGrid();
 
 // create a function to add a gray background color
 const colorGray = (el) => {
@@ -21,9 +49,13 @@ const colorGray = (el) => {
 };
 
 // create a hover effect for the grid div elements
-const divs = document.querySelectorAll(".square");
-divs.forEach((div) => {
-  div.addEventListener("mouseover", (e) => {
-    colorGray(e.target);
+const setHoverEffect = () => {
+  const divs = document.querySelectorAll(".square");
+  divs.forEach((div) => {
+    div.addEventListener("mouseover", (e) => {
+      colorGray(e.target);
+    });
   });
-});
+};
+
+setHoverEffect();
